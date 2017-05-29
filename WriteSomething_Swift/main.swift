8d4f517174daf8a43b234e16075fb8fa430947c6
leonguevara <leon.guevara@outlook.com>
2017-05-29 12:01:18 -0500

    Initial Commit

---
+++ WriteSomething_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WriteSomething_Swift
+//
+//  Created by León Felipe Guevara Chávez on 5/29/17.
+//  Copyright © 2017 León Felipe Guevara Chávez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
